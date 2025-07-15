73c64503cd0b040ca8e336653cc67025503df850
alexander <alexanderkir1980@gmail.com>
2025-07-15 11:32:10 +0300

    fix index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./components/App.jsx";
-import DataProvider from "./providers/dataProvider.js";
 
 const mountNode = document.getElementById("root");
 const root = ReactDOM.createRoot(mountNode);
-root.render(
-  <DataProvider>
-    <App />
-  </DataProvider>,
-);
+root.render(<App />);
